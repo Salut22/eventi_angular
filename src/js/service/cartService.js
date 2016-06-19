b3de8c65bab82d0ebe5a16291100f74fd1a91441
claudio.saluti <claudio.saluti@studenti.unicam.it>
2016-06-19 20:05:38 +0200

    aggiunta funzione che aggiorna eventi carrello e restituisce contatore

--- src/js/service/cartService.js
+++ src/js/service/cartService.js
@@ -1,12 +1,8 @@
 angular.module('app.serviceModule')
-
-
-
-
 .service('CartService', ['$q','$http','GrowlService', function ($q, $http, GrowlService) 
   {   // initialization
-	  	 		  		
-	   		  		
+	this.cart;  	 		  		
+    var self = this;
     this.getById=function(id)
     {
         var deferred = $q.defer();
@@ -16,7 +12,7 @@ angular.module('app.serviceModule')
 	   	$http.get(query)
         .success(function(doc)
         {
-          console.log(doc);
+          self.cart = doc.result;
           deferred.resolve(doc);
         })
         .error(function(doc, status, headers, config) 
@@ -24,8 +20,39 @@ angular.module('app.serviceModule')
 		  deferred.reject({'error':data, 'status':status});
 		}
 		);
-        	  return deferred.promise; 	
+        return deferred.promise; 	
 
+    }
+    this.getCounter=function (id)
+    {
+        for (i in self.cart.prodotto)
+        {
+            if (id ==self.cart.prodotto[i])
+            {
+                return self.cart.prodotto[i]details.quantita;
+            }
+                
+        }
+        
+    }
+    this.addToCart(cart,userId)
+    {
+        var deferred = $q.defer();
+        console.log('id '+userId+' cart '+cart);
+        var query = 'http://localhost:8080/addEvent;
+        console.log(query);
+	   	$http.post(query, cart)
+        .success(function(doc)
+        {
+          self.cart = doc.result;
+          deferred.resolve(doc);
+        })
+        .error(function(doc, status, headers, config) 
+        {
+		  deferred.reject({'error':data, 'status':status});
+		}
+		);
+        return deferred.promise;
     }
 }])
 
