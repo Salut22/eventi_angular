9b582847891c6704f86c56c202e7512ff3eb1dd4
Tony Ciarrocchi <Tony Ciarrocchi>
2016-06-19 20:05:12 +0200

    aggiunte funzioni per il carrello dai dettagli

--- src/js/controller/dettagliCtrl.js
+++ src/js/controller/dettagliCtrl.js
@@ -1,12 +1,15 @@
 angular.module('app')
 
-.controller("dettagliCtrl" ,['$http','CurrentPoiService','CurrentUserService','$location', function ($http,CurrentPoiService,CurrentUserService,$location) 
+.controller("dettagliCtrl" ,['$http','CurrentPoiService','CurrentUserService','CartService', function ($http,CurrentPoiService,CurrentUserService,CartService) 
 {
     this.poi;
+    this.prezzo;
+    this.userId;
     var self=this;
     
     self.poi=CurrentPoiService.currentEvent();
-    console.log(self.poi);
+    self.userId = CurrentUserService.getUserId();
+    
         
     
     $("#title").html('titolo evento '+self.poi.properties.title);
@@ -15,10 +18,68 @@ angular.module('app')
     $("#email").html('email '+self.poi.details.contact.email);
     $("#website").html('sito web '+self.poi.details.contact.website);
     $("#phone").html('telefono '+self.poi.details.contact.phone);
-    $("#price").html(self.poi.details.price.prc_info);
+    if(self.poi.details.price)
+    {
+        self.prezzo=self.poi.details.price.prc_info;
+        $("#price").html(self.poi.details.price.prc_info);
+    }
+    else
+    {
+        self.prezzo=0;
+        $("#price").html("l'evento è gratuito");
+    }
+    
     $("#dateFrom").html('dal '+self.poi.properties.dataFromFormatted);
     $("#dateTo").html('al '+self.poi.properties.dateToFormatted);
-    $("#desc").html(self.poi.details.desc);
+    if(self.poi.details.desc)
+    {$("#desc").html(self.poi.details.desc);}
     
+    
+    this.addToCart=function()
+    {
+        
+        var poi    = self.poi;
+        var quantita= CartService.getCounter(poi._id);
+        var cart=
+            {
+                'prodotto':
+                [{
+                    'properties':
+                    {
+                      'title'   :poi.properties.title,
+                      'dateFrom':poi.properties.dateFrom,
+                      'dateTo'  :poi.properties.dateTo
+                    },
+                    'details':
+                    {
+                      'idEvento':poi._id,
+                      'photo'   :poi.details['ph-primary'],
+                      'price'   :self.prezzo,
+                      'quantita':qauntita+1
+                    }
+                }]  
+            };
+        CartService.addToCart(cart,self.userId)
+        .then(function(data)
+        {
+          console.log('aggiunto con successo');    
+        })
+        .catch(function(err)
+        {
+            console.log(err);    
+        });
+    }
+    this.deleteToCart=function()
+    {
+        CartService.deleteToCart(self.userId,self.poi._id)
+        .then(function(data)
+        {
+            console.log('evento cancellato dal carrello');   
+        })
+        .catch(function(err)
+        {
+            console.log(err);
+        });
+    }
   
 }]);
